75343d9003151a25ce0030fbe578bc004468a725
charbel-raphael.segerie <charbel-raphael.segerie@kili-technology.com>
2022-03-25 08:58:00 +0100

    Mnist experiment

--- prioritize.py
+++ prioritize.py
@@ -224,16 +224,22 @@ def main(
         mnist_images_df = mnist.data
         mnist_labels = mnist.target
 
+        #  takes a random sample of the images
+        mask = np.random.choice(mnist_images_df.shape[0], 1000)
+        mnist_images_df = mnist_images_df[mask]
+        mnist_labels = mnist_labels[mask]
+
         # delete 9/10th of the images with an 8 label
-        mnist_images_df = mnist_images_df[mnist_labels != 8]
-        mnist_labels = mnist_labels[mnist_labels != 8]
+        idx_8 = np.where(mnist_labels == 8)[0]
+        almost_all_idx_8 = np.random.choice(
+            idx_8, size=int(len(idx_8) / 10), replace=False
+        )
+        mnist_images_df = mnist_images_df[almost_all_idx_8]
+        mnist_labels = mnist_labels[almost_all_idx_8]
 
         from PIL import Image
 
-        mnist_images_np = mnist_images_df.to_numpy().reshape(
-            (-1, 28, 28, 1)
-        )
-
+        mnist_images_np = mnist_images_df.to_numpy().reshape((-1, 28, 28, 1))
 
         pil_images = [Image.fromarray(image) for image in mnist_images_np]
 
