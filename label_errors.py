cf91ff08626dda0b065c0eb55e10d0732d563c72
Gheorghe Tutunaru <gheorghe.tutunaru@kili-technology.com>
2022-04-14 16:13:49 +0200

    feat(ML-78): add script for getting incorrectly labeled assets using cleanlab

---
+++ label_errors.py
@@ -0,0 +1,184 @@
+import os
+import json
+import requests
+import shutil
+import time
+
+import click
+from kili.client import Kili
+from tqdm.auto import tqdm
+
+from utils.cleanlab.train_cleanlab import train_and_get_error_labels
+from utils.constants import (
+    HOME,
+    ContentInput,
+    InputType,
+    MLTask,
+    ModelName,
+)
+from utils.helpers import (
+    build_model_repository_path,
+    get_assets,
+    get_project,
+    kili_print,
+    parse_label_types,
+    set_default,
+)
+
+
+@click.command()
+@click.option(
+    "--api-endpoint",
+    default="https://cloud.kili-technology.com/api/label/v2/graphql",
+    help="Kili Endpoint",
+)
+@click.option("--api-key", default=os.environ.get("KILI_API_KEY"), help="Kili API Key")
+@click.option("--cv-folds", default=4, type=int, help="Number of CV folds to use")
+@click.option(
+    "--clear-dataset-cache",
+    default=False,
+    is_flag=True,
+    help="Tells if the dataset cache must be cleared",
+)
+@click.option(
+    "--label-types",
+    default="DEFAULT",
+    help=(
+        "Comma separated list Kili specific label types to select (among DEFAULT,"
+        " REVIEW, PREDICTION)"
+    ),
+)
+@click.option("--max-assets", default=None, type=int, help="Maximum number of assets to consider")
+@click.option(
+    "--model-name",
+    default=None,
+    help="Model name (one of efficientnet_b0, resnet50)",
+)
+@click.option("--project-id", default=None, help="Kili project ID")
+@click.option(
+    "--training-epochs",
+    default=10,
+    type=int,
+    help="Number of epochs to train each CV fold",
+)
+def main(
+    api_endpoint: str,
+    api_key: str,
+    cv_folds: int,
+    clear_dataset_cache: bool,
+    label_types: str,
+    max_assets: int,
+    model_name: str,
+    project_id: str,
+    training_epochs: int,
+):
+    """
+    Main method for detecting incorrect labeled assets in a Kili project.
+    It downloads the assets, trains a classification neural network with CV and then by
+    using the Cleanlab library we get the wrong labels. The concerned asset IDs are then
+    stored in a file, but also a metadata (cleanlab_error: true) is uploaded to Kili to
+    easily filter them later in the app.
+    """
+
+    kili = Kili(api_key=api_key, api_endpoint=api_endpoint)
+    input_type, jobs = get_project(kili, project_id)
+
+    for job_name, job in jobs.items():
+        content_input = job.get("content", {}).get("input")
+        ml_task = job.get("mlTask")
+        if (
+            content_input == ContentInput.Radio
+            and input_type == InputType.Image
+            and ml_task == MLTask.Classification
+        ):
+            job_path = build_model_repository_path(HOME, project_id, job_name, "")
+            data_path = os.path.join(job_path, "data")
+            model_path = os.path.join(job_path, "model")
+
+            if clear_dataset_cache and os.path.exists(job_path) and os.path.isdir(job_path):
+                kili_print("Dataset cache for this project is being cleared.")
+                shutil.rmtree(job_path)
+
+            os.makedirs(job_path, exist_ok=True)
+            os.makedirs(data_path, exist_ok=True)
+            os.makedirs(model_path, exist_ok=True)
+
+            kili_print("Downloading datasets from Kili")
+            assets = get_assets(kili, project_id, parse_label_types(label_types), max_assets)
+            if len(assets) == 0:
+                raise Exception("No asset in dataset, exiting...")
+
+            for asset in tqdm(assets):
+                tic = time.time()
+                n_try = 0
+                while n_try < 20:
+                    try:
+                        img_data = requests.get(
+                            asset["content"],
+                            headers={
+                                "Authorization": f"X-API-Key: {api_key}",
+                            },
+                        ).content
+                        break
+                    except Exception:
+                        time.sleep(1)
+                        n_try += 1
+                img_path = os.path.join(
+                    data_path, asset["labels"][0]["jsonResponse"][job_name]["categories"][0]["name"]
+                )
+                os.makedirs(img_path, exist_ok=True)
+                with open(os.path.join(img_path, asset["id"] + ".jpg"), "wb") as handler:
+                    handler.write(img_data)
+                toc = time.time() - tic
+                throttling_per_call = 60.0 / 250  # Kili API calls are limited to 250 per minute
+                if toc < throttling_per_call:
+                    time.sleep(throttling_per_call - toc)
+
+            model_name = set_default(
+                model_name,
+                ModelName.EfficientNetB0,
+                "model_name",
+                [ModelName.EfficientNetB0, ModelName.Resnet50],
+            )
+
+            found_errors = train_and_get_error_labels(
+                cv_n_folds=cv_folds,
+                data_dir=data_path,
+                model_dir=model_path,
+                model_name=model_name,
+                training_epochs=training_epochs,
+            )
+
+            print()
+            kili_print("Number of wrong labels found: ", len(found_errors))
+
+            if found_errors:
+                found_errors_dict = {"assetIds": found_errors}
+                found_errors_json = json.dumps(found_errors_dict, sort_keys=True, indent=4)
+                if found_errors_json is not None:
+                    json_path = os.path.join(job_path, "error_labels.json")
+                    with open(json_path, "wb") as output_file:
+                        output_file.write(found_errors_json.encode("utf-8"))
+                        kili_print("Asset IDs of wrong labels written to: ", json_path)
+
+                kili_print("Updating metadatas for the concerned assets")
+                first = min(100, len(found_errors))
+                for skip in tqdm(range(0, len(found_errors), first)):
+                    error_assets = kili.assets(
+                        asset_id_in=found_errors[skip : skip + first], fields=["id", "metadata"]
+                    )
+                    asset_ids = [asset["id"] for asset in error_assets]
+                    new_metadatas = [asset["metadata"] for asset in error_assets]
+
+                    for meta in new_metadatas:
+                        meta["cleanlab_error"] = True
+
+                    kili.update_properties_in_assets(
+                        asset_ids=asset_ids, json_metadatas=new_metadatas
+                    )
+        else:
+            kili_print("not implemented yet")
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/cleanlab/train_cleanlab.py
@@ -0,0 +1,271 @@
+import copy
+import os
+import time
+
+from cleanlab.pruning import get_noise_indices
+import numpy as np
+from sklearn.model_selection import StratifiedKFold, train_test_split
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+from torchvision import datasets, models, transforms
+
+from utils.constants import ModelName
+
+
+def train_model(
+    model, criterion, optimizer, scheduler, dataloaders, device, dataset_sizes, num_epochs=10
+):
+    """
+    Method that trains the given model and return the best one found in the given epochs
+    """
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        print("-" * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == "train":
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+            # deep copy the model
+            if phase == "val" and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
+    print(f"Best val Acc: {best_acc:4f}")
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+def get_probs(loader, model):
+    """
+    Method to compute the probabilities for all classes for the assets in the holdout set
+    """
+    # Switch to evaluate mode.
+    model.eval()
+    n_total = len(loader.dataset.imgs) / float(loader.batch_size)
+    outputs = []
+    with torch.no_grad():
+        for i, (input, target) in enumerate(loader):
+            print("\rComplete: {:.1%}".format(i / n_total), end="")
+            if torch.cuda.is_available():
+                input = input.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+
+            # compute output
+            outputs.append(model(input))
+
+    # Prepare outputs as a single matrix
+    probs = np.concatenate([torch.nn.functional.softmax(z, dim=1).cpu().numpy() for z in outputs])
+
+    return probs
+
+
+def combine_folds(data_dir, model_dir, num_classes=10, nb_folds=4, seed=42):
+    """
+    Method that combines the probabilities from all the holdout sets into a single file
+    """
+    destination = os.path.join(model_dir, "train_model_intel_pyx.npy")
+    print()
+    print("This method will overwrite file: {}".format(destination))
+    # Prepare labels
+    labels = [label for _, label in datasets.ImageFolder(data_dir).imgs]
+    # Initialize pyx array (output of trained network)
+    pyx = np.empty((len(labels), num_classes))
+
+    # Split train into train and holdout for each cv_fold.
+    kf = StratifiedKFold(n_splits=nb_folds, shuffle=True, random_state=seed)
+    for k, (_, cv_holdout_idx) in enumerate(kf.split(range(len(labels)), labels)):
+        probs_path = os.path.join(model_dir, "model_fold_{}__probs.npy".format(k))
+        probs = np.load(probs_path)
+        pyx[cv_holdout_idx] = probs[:, :num_classes]
+    print("Writing final predicted probabilities.")
+    np.save(destination, pyx)
+
+    # Compute overall accuracy
+    print("Computing Accuracy.", flush=True)
+    acc = sum(np.array(labels) == np.argmax(pyx, axis=1)) / float(len(labels))
+    print("Accuracy: {:.25}".format(acc))
+
+    return destination
+
+
+def train_and_get_error_labels(
+    cv_n_folds,
+    data_dir,
+    model_dir,
+    model_name,
+    training_epochs,
+    cv_seed=42,
+):
+    """
+    Main method that trains the model on the assets that are in data_dir, computes the
+    incorrect labels using Cleanlab and returns the IDs of the concerned assets.
+    """
+    # To set to False if the input size varies a lot and you see that the training takes
+    # too much time
+    cudnn.benchmark = True
+
+    data_transforms = {
+        "train": transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ]
+        ),
+        "val": transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ]
+        ),
+    }
+
+    original_image_datasets = {
+        x: datasets.ImageFolder(data_dir, data_transforms[x]) for x in ["train", "val"]
+    }
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    for cv_fold in range(cv_n_folds):
+        labels = [label for img, label in datasets.ImageFolder(data_dir).imgs]
+        # Split train into train and holdout for particular cv_fold.
+        kf = StratifiedKFold(n_splits=cv_n_folds, shuffle=True, random_state=cv_seed)
+        cv_train_idx, cv_holdout_idx = list(kf.split(range(len(labels)), labels))[cv_fold]
+        # Separate datasets
+        np.random.seed(cv_seed)
+        train_idx, val_idx = train_test_split(cv_train_idx, test_size=0.2)
+        image_datasets = copy.deepcopy(original_image_datasets)
+        holdout_dataset = copy.deepcopy(image_datasets["val"])
+        holdout_dataset.imgs = [holdout_dataset.imgs[i] for i in cv_holdout_idx]
+        holdout_dataset.samples = holdout_dataset.imgs
+        image_datasets["train"].imgs = [image_datasets["train"].imgs[i] for i in train_idx]
+        image_datasets["train"].samples = image_datasets["train"].imgs
+        image_datasets["val"].imgs = [image_datasets["val"].imgs[i] for i in val_idx]
+        image_datasets["val"].samples = image_datasets["val"].imgs
+
+        print(f"\n\nCV Fold: {cv_fold+1}/{cv_n_folds}")
+        print(f"Train size: {len(image_datasets['train'].imgs)}")
+        print(f"Validation size: {len(image_datasets['val'].imgs)}")
+        print(f"Holdout size: {len(holdout_dataset.imgs)}")
+        print()
+
+        dataloaders = {
+            x: torch.utils.data.DataLoader(
+                image_datasets[x], batch_size=64, shuffle=True, num_workers=4
+            )
+            for x in ["train", "val"]
+        }
+        dataset_sizes = {x: len(image_datasets[x]) for x in ["train", "val"]}
+        class_names = image_datasets["train"].classes
+
+        if model_name == ModelName.EfficientNetB0:
+            model_ft = models.efficientnet_b0(pretrained=True)
+            num_ftrs = model_ft.classifier[1].in_features
+            model_ft.classifier[1] = nn.Linear(num_ftrs, len(class_names))
+        elif model_name == ModelName.Resnet50:
+            model_ft = models.resnet50(pretrained=True)
+            num_ftrs = model_ft.fc.in_features
+            model_ft.fc = nn.Linear(num_ftrs, len(class_names))
+
+        model_ft = model_ft.to(device)
+
+        criterion = nn.CrossEntropyLoss()
+
+        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+        # Decay LR by a factor of 0.1 every 7 epochs
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+        model_ft = train_model(
+            model_ft,
+            criterion,
+            optimizer_ft,
+            exp_lr_scheduler,
+            dataloaders,
+            device,
+            dataset_sizes,
+            num_epochs=training_epochs,
+        )
+        # torch.save(model_ft.state_dict(), os.path.join(model_dir, f'model_{cv_fold}.pt'))
+
+        holdout_loader = torch.utils.data.DataLoader(
+            holdout_dataset,
+            batch_size=64,
+            shuffle=False,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+        probs = get_probs(holdout_loader, model_ft)
+        probs_path = os.path.join(model_dir, "model_fold_{}__probs.npy".format(cv_fold))
+        np.save(probs_path, probs)
+
+    psx_path = combine_folds(
+        data_dir=data_dir, model_dir=model_dir, num_classes=len(class_names), seed=cv_seed
+    )
+
+    psx = np.load(psx_path)
+    train_imgs = datasets.ImageFolder(data_dir).imgs
+
+    noise_indices = get_noise_indices(labels, psx, sorted_index_method="normalized_margin")
+
+    noise_paths = []
+    for idx in noise_indices:
+        noise_paths.append(os.path.basename(train_imgs[idx][0])[:-4])
+
+    return noise_paths
