4c1b0b829c255650d6c42842df0257be5f695744
charbel-raphael.segerie <charbel-raphael.segerie@kili-technology.com>
2022-05-25 19:26:27 +0200

    feat(ML-292): kiliautoml train --help

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="kiliautoml",
     version="0.1.0",
-    py_modules=["train", "predict", "label_errors"],
+    py_modules=["main"],
     install_requires=[
         "Click",
         "cleanlab>=2.0.0",
@@ -27,9 +27,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "kiliautoml_train = train:main",
-            "kiliautoml_predict = predict:main",
-            "kiliautoml_label_errors = label_errors:main",
+            "kiliautoml = main:group",
         ],
     },
 )
