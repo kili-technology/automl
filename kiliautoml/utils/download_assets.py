3e47b98a3207238645b49efe4eccfa12ff3b9a02
Theo Dullin <theo@kili-technology.com>
2022-12-07 15:18:59 +0100

    fix: add empty extension when searching for assets

--- kiliautoml/utils/download_assets.py
+++ kiliautoml/utils/download_assets.py
@@ -218,3 +218,4 @@ def _get_extensions_for_type(general_type):
     for ext in mimetypes.types_map:
         if mimetypes.types_map[ext].split("/")[0] == general_type:
             yield ext
+    yield ""
